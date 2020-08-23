235d011a278a635c5fc56714e1acc40ef84c9e51
BGNDEE001 <BGNDEE001@myuct.ac.za>
2020-08-23 19:01:20 +0200

    Change saveVideo method to include other methods.

--- app/src/main/java/com/example/danceit/Model/LibraryModel.java
+++ app/src/main/java/com/example/danceit/Model/LibraryModel.java
@@ -5,9 +5,11 @@ import java.io.ObjectOutputStream;
 public class LibraryModel {
 
    private ArrayList<Video> videoList;
+   private String storage_filename;
 
     public LibraryModel() {
         videoList = new ArrayList<Video>();
+        storage_filename = "URL_storage.txt";
     }
 
     public void addVideo(Video video) {
@@ -15,21 +17,27 @@ public class LibraryModel {
     }
 
     public void saveVideo() {
-        String filename = "URL_storage.txt";
         try {
-            for(int i = 0; i < videoList.size(); i++) {
-                FileOutputStream fileOutput = new FileOutputStream(filename, true);
-                ObjectOutputStream objectOutput = new ObjectOutputStream(fileOut);
-                objectOutput.writeObject(videoList.indexOf(i));
-                objectOutput.close();
+
+            File file = new File(storage_filename);
+
+            if(!file.exists() || file.length() == 0)
+                writeFileObj(videoList);
+
+            else {
+                ArrayList<Video> videoObjs = readFileObj(storage_filename);
+                for(Video videoObj : videoList)
+                    videoObjs.add(videoObj);
+                writeFileObj(videoObjs);
             }
             videoList.clear();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
     }
+
+
 
 
 }
