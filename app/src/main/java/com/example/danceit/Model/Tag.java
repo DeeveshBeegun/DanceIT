a6c9db924b1727b47cefdffd4370e2ed390bd970
Bohlale Motsieloa <bohlalemotsieloa@gmail.com>
2020-08-25 21:20:49 +0000

    Update app/src/main/java/com/example/danceit/Model/suggestedTag.java, app/src/main/java/com/example/danceit/Model/Tag.java, app/src/main/java/com/example/danceit/Model/Video.java files

---
+++ app/src/main/java/com/example/danceit/Model/Tag.java
@@ -0,0 +1,78 @@
+/**
+ * This class represents the model for the Tag object
+ *
+ * @author Bohlale Motsieloa (MTSBOH002)
+ * @date: 24/08/2020
+ * @version: 0.0 (Prototype)
+ */
+
+package com.example.danceit.Model;
+ 
+import java.util.ArrayList; 
+
+public class Tag {
+    private User owner;
+    private String description;
+    private boolean hasComplaint;
+
+
+    /*Constructor*/
+    public Tag(User owner, String description, boolean hasComplaint) {
+        this.owner = owner;
+        this.description = description;
+        this.hasComplaint = hasComplaint;
+    }
+
+    /*Get and Set Methods*/
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public boolean getHasComplaint() {
+        return hasComplaint;
+    }
+
+    public void setHasComplaint (boolean hasComplaint) {
+        this.hasComplaint = hasComplaint;
+    }
+
+
+    /*Overriding equals method to allow correct Tag object comparison*/
+    @Override
+    public boolean equals(Object object) 
+    { 
+          
+        if(this == object){
+           return true;  
+        } 
+           
+        if(object == null || object.getClass()!= this.getClass()) {
+            return false;
+        }    
+           
+        Tag tag = (Tag) object; 
+          
+        return tag.description.equals(this.description) ; 
+    }
+
+    /*Overriding hashCode method to allow correct Tag object comparison*/
+    @Override
+    public int hashCode() 
+    {  
+        return this.description.hashCode(); 
+    }  
+      
+     
+}
---
+++ app/src/main/java/com/example/danceit/Model/Video.java
@@ -0,0 +1,91 @@
+/**
+ * This class represents the model for the Video Object.
+ * @author Bohlale Motsieloa (MTSBOH002)
+ * @date: 24/08/2020
+ * @version: 0.0 (Prototype)
+ */
+ 
+package com.example.danceit.Model;
+ 
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Video implements Serializable {
+    private User videoUploader;
+    private String URL;
+    private ArrayList<Tag> Tags;
+    private boolean Private;
+
+
+    /*Constructor*/
+    public Video(User videoUploader, String URL, ArrayList<Tag> Tags, boolean Private) {
+        this.videoUploader = videoUploader;
+        this.URL = URL;
+        this.Tags = Tags;
+        this.Private = Private;
+    }
+
+    /*Get and Set Methods*/
+    public User getVideoUploader() {
+        return videoUploader;
+    }
+
+    public void setVideoUploader(User videoUploader) {
+        this.videoUploader = videoUploader;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public ArrayList<Tag> getTagList() {
+        return Tags;
+    }
+
+    public void setTagList(ArrayList<Tag> Tags) {
+        this.Tags = Tags;
+    }
+
+    public boolean getPrivate() {
+        return Private;
+    }
+
+    public void setPrivate(boolean Private) {
+        this.Private = Private;
+    }
+    
+    /**
+     * This method adds a Tag to the Video object.
+     * If a tag is in the list, false is returned to indicate no addition happened.
+     * @param Tags arraylist containing Tag objects.
+     */
+     public boolean addTag(String tag) {
+         Tag tag1 = new Tag(this.videoUploader, tag, this, false); // Creating new Tag
+         if ( Tags.contains(tag1) ){
+            return false; // Shows tag description already exists 
+         }
+         else{
+            Tags.add(tag1);
+            return true;
+         }
+        
+     }
+     
+     /**
+     * This method finds if the Video object has a certain tag description.
+     * returns false if a video does not have tags 
+     * @param Tags arraylist containing Tag objects.
+     */
+     
+     public boolean findTag(String tag) { 
+         if( Tags.isEmpty() ){
+            return false;
+         }
+         return Tags.contains(new Tag(this.videoUploader, tag, this, false));
+     }
+  
+}
