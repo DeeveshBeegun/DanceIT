043eb2a37ed3046f1f8b976c63c1a00fd47afb45
BGNDEE001 <BGNDEE001@myuct.ac.za>
2020-08-28 22:15:50 +0200

    Add videoRepository class for database management.

---
+++ app/src/main/java/com/example/danceit/Database/VideoRepositoty.java
@@ -0,0 +1,45 @@
+package com.example.danceit.Database;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.danceit.Model.Video;
+
+import java.util.List;
+
+public class VideoRepositoty {
+    private Video_Dao video_dao;
+    private LiveData<List<Video>> videos;
+
+    public VideoRepositoty(Application application) {
+        Video_database video_database = Video_database.getInstance(application);
+        video_dao = video_database.video_dao();
+        videos = video_dao.getAll();
+
+    }
+
+    public void insert_video(Video video) {
+        new InsertAsync(video_dao).execute(video);
+    }
+
+    public LiveData<List<Video>> getAll() {
+        return videos;
+    }
+
+    private static class InsertAsync extends AsyncTask<Video, Void, Void> {
+       private Video_Dao video_dao;
+
+       private InsertAsync(Video_Dao video_dao) {
+           this.video_dao = video_dao;
+       }
+
+        @Override
+        protected Void doInBackground(Video... videos) {
+           video_dao.insert_video(videos[0]);
+            return null;
+        }
+    }
+
+}
