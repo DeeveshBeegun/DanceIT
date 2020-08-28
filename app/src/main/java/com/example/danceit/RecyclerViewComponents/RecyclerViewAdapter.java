d20b6edb0f39b56d8d81554a919b406e2e051e92
Claudious Nhemwa <nhmcla003@myuct.ac.za>
2020-08-29 01:39:45 +0200

    Added feature to be able to forward video data to the clipboard
    Signed-off-by: “Claudious <nhmcla003@myuct.ac.za>

--- app/src/main/java/com/example/danceit/RecyclerViewComponents/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/danceit/RecyclerViewComponents/RecyclerViewAdapter.java
@@ -1,14 +1,19 @@
 package com.example.danceit.RecyclerViewComponents;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.chip.Chip;
 import android.support.design.chip.ChipGroup;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.danceit.Model.Video;
 import com.example.danceit.R;
@@ -32,6 +37,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             textView = (TextView) v.findViewById(R.id.text_recycler);
             chipGroup=(ChipGroup) v.findViewById(R.id.chipGroup);
             context=v.getContext();
+
+
         }
     }
 
@@ -43,10 +50,34 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public MyViewHolder onCreateViewHolder(@NonNull final ViewGroup viewGroup, final int i) {
         // create a new view
         View v = (View) LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.recycler_view_layout, viewGroup, false);
+        //feature on click for share button
+        AppCompatImageButton appCompatImageButton= v.findViewById(R.id.shareButton);
+        appCompatImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get all video data
+                String dataOut=dataset.get(i).getUrl().toString();
+                dataOut+="\nTags\n";
+
+                for (int j = 0; j <dataset.get(i).getTag_list().size() ; j++) {
+                    dataOut+=dataset.get(i).getTag_list().get(j).getDescription()+" ";
+
+                }
+
+                //copy video data to clip board
+                ClipboardManager clipboardManager=(ClipboardManager) viewGroup.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData=ClipData.newPlainText("video data",dataOut);
+                clipboardManager.setPrimaryClip(clipData);
+
+                Snackbar snackbar = Snackbar
+                        .make(viewGroup.getRootView(),"Data saved to clipboard!!!", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
 
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
