fc8ec10f46c77701016e50e7d39bfcee2c94a07a
Claudious Nhemwa <nhmcla003@myuct.ac.za>
2020-08-30 16:21:02 +0200

    Made sure the onlick for and redirecting to youtube with the right
    Signed-off-by: “Claudious <nhmcla003@myuct.ac.za>

--- app/src/main/java/com/example/danceit/RecyclerViewComponents/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/danceit/RecyclerViewComponents/RecyclerViewAdapter.java
@@ -3,15 +3,6 @@ package com.example.danceit.RecyclerViewComponents;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-
-import androidx.annotation.NonNull;
-import com.google.android.material.chip.Chip;
-import com.google.android.material.chip.ChipGroup;
-
-import androidx.appcompat.widget.AppCompatImageButton;
-import androidx.lifecycle.LiveData;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -19,13 +10,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.danceit.Model.Video;
 import com.example.danceit.R;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
@@ -64,59 +60,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         View v = (View) LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.recycler_view_layout, viewGroup, false);
 
-        ImageView imageView=(ImageView) v.findViewById(R.id.image_view);
-        imageView.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                System.out.print(i +dataset.get(i).getUrl().toString());
-                Intent viewIntent =
-                        new Intent("android.intent.action.VIEW",
-                                Uri.parse("https://www.youtube.com/watch?v=QkoCpVyXAmo"));
-
-                try{
-                v.getContext().startActivity(viewIntent);
-                }catch (Exception e){
-
-                    Snackbar snackbar = Snackbar
-                            .make(viewGroup.getRootView(),"Invalid URL "+ String.valueOf(i)+ " "+   dataset.get(i).getUrl().trim(), Snackbar.LENGTH_LONG);
-                    snackbar.show();
-                }
-
-            }
-        });
-
-        //feature on click for share button
-        AppCompatImageButton appCompatImageButton= v.findViewById(R.id.shareButton);
-        appCompatImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //get all video data
-                String dataOut=dataset.get(i).getUrl().toString();
-                dataOut+="\nTags\n";
-
-                for (int j = 0; j <dataset.get(i).getTag_list().size() ; j++) {
-                    dataOut+=dataset.get(i).getTag_list().get(j).getDescription()+" ";
-
-                }
-
-                //copy video data to clip board
-                ClipboardManager clipboardManager=(ClipboardManager) viewGroup.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData=ClipData.newPlainText("video data",dataOut);
-                clipboardManager.setPrimaryClip(clipData);
-
-                Snackbar snackbar = Snackbar
-                        .make(viewGroup.getRootView(),"Data saved to clipboard!!!", Snackbar.LENGTH_LONG);
-                snackbar.show();
-            }
-        });
-
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+    public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, final int i) {
 
         myViewHolder.textView.setText((CharSequence) dataset.get(i).getUrl());
         myViewHolder.chipGroup.animate();
@@ -130,6 +79,50 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             myViewHolder.chipGroup.addView(temp);
 
         }
+
+        //feature on click for share button
+        AppCompatImageButton appCompatImageButton= myViewHolder.textView.getRootView().findViewById(R.id.shareButton);
+        appCompatImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get all video data
+                String dataOut=dataset.get(i).getUrl().toString();
+                dataOut+="\nTags\n";
+                for (int j = 0; j <dataset.get(i).getTag_list().size() ; j++) {
+                    dataOut+=dataset.get(i).getTag_list().get(j).getDescription()+" ";
+                }
+                //copy video data to clip board
+                ClipboardManager clipboardManager=(ClipboardManager) myViewHolder.context.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData=ClipData.newPlainText("video data",dataOut);
+                clipboardManager.setPrimaryClip(clipData);
+                Snackbar snackbar = Snackbar
+                        .make(myViewHolder.textView.getRootView(),"Data saved to clipboard!!!", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
+
+
+        ImageView imageView=(ImageView) myViewHolder.itemView.getRootView().findViewById(R.id.image_view);
+        imageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.print(i +dataset.get(i).getUrl().toString());
+                Intent viewIntent =
+                        new Intent("android.intent.action.VIEW",
+                                Uri.parse("https://www.youtube.com/watch?v=QkoCpVyXAmo"));
+
+                try{
+                    v.getContext().startActivity(viewIntent);
+                }catch (Exception e){
+
+                    Snackbar snackbar = Snackbar
+                            .make(myViewHolder.itemView.getRootView(),"Invalid URL "+ String.valueOf(i)+ " "+   dataset.get(i).getUrl().trim(), Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
+
+            }
+        });
 
     }
 
