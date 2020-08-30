1d817ac6efe407ca5dd07d29bf6b1019ef0bf2b1
BGNDEE001 <BGNDEE001@myuct.ac.za>
2020-08-30 07:05:20 +0200

    Fix extra white space in tags on the recyclerView.

--- app/src/main/java/com/example/danceit/AddVideoActivity.java
+++ app/src/main/java/com/example/danceit/AddVideoActivity.java
@@ -63,9 +63,9 @@ public class AddVideoActivity extends AppCompatActivity {
         ArrayList<Tag> tag_lists = new ArrayList<Tag>();
         String[] description = textInputTags.split("#");
 
-        for (int i = 0; i < description.length; i++) {
-            Tag tag = new Tag(new User("username", "password"), description[i], false);
-            tag_lists.add(tag);
+        for (int i = 1; i < description.length; i++) {
+                Tag tag = new Tag(new User("username", "password"), description[i], false);
+                tag_lists.add(tag);
         }
         return tag_lists;
     }
