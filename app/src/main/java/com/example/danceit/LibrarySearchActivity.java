6c9e505d368628ba1d1b5cf507ad8056f2e089d9
bohlalemotsieloa <bohlalemotsieloa@gmail.com>
2020-10-10 12:59:38 +0200

    Search that works without tag object and FirebaseRecyclerView adapter.

--- app/src/main/java/com/example/danceit/LibrarySearchActivity.java
+++ app/src/main/java/com/example/danceit/LibrarySearchActivity.java
@@ -1,70 +1,105 @@
-///**
-// * This activity handles the library search queries
-// *
-// * @author Bohlale Motsieloa (MTSBOH002)
-// * @date: 29/09/2020
-// * @version: 1.0
-// */
-//package com.example.danceit;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//
-////import com.example.danceit.Database.VideoViewModel;
-////import com.example.danceit.Database.Video_database;
-//import com.example.danceit.Model.Search;
-//import com.example.danceit.Model.Tag;
-//import com.example.danceit.Model.User;
-//import com.example.danceit.Model.Video;
-////import com.example.danceit.RecyclerViewComponents.RecyclerViewAdapter;
-//import com.google.android.material.floatingactionbutton.FloatingActionButton;
-//import com.google.android.material.snackbar.Snackbar;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.appcompat.widget.Toolbar;
-//import androidx.recyclerview.widget.LinearLayoutManager;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import android.view.View;
-//import android.widget.ArrayAdapter;
-//import android.widget.ListView;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class LibrarySearchActivity extends AppCompatActivity {
-//
-//    //RecyclerViewAdapter mAdapter;
-//    Tag [] searchTags;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_library_search);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        // Getting intent, user search query and all the videos in the user's library from the Main Activity
-//        Intent intent = getIntent();
-//        String[] searchKeywords = intent.getExtras().getStringArray("Search Keywords");
-//        List<Video> allVideos = intent.getExtras().getParcelableArrayList("Videos");
-//
-//        //Creating temporary tags from the user's search query words
-//        searchTags = new Tag[searchKeywords.length];
-//
-//        for (int i=0; i<searchKeywords.length; i++ ){
-//            String tag = searchKeywords[i];
-//            Tag newTag = new Tag(new User(tag, "d"), tag, false );
-//            searchTags[i]=newTag;
-//        }
-//
-//        //Create Search object to get search results and set the adapter
-//        Search search = new Search (searchTags, allVideos);
-//        ArrayList<Video> searchResults = search.searchResults();
-//
-//        //mAdapter = new RecyclerViewAdapter(searchResults);
-//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-//       // recyclerView.setAdapter(mAdapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-//    }
-//}
+/**
+ * This activity handles the library search queries
+ *
+ * @author Bohlale Motsieloa (MTSBOH002)
+ * @date: 29/09/2020
+ * @version: 1.0
+ */
+package com.example.danceit;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.example.danceit.Model.Search;
+import com.example.danceit.Model.Tag;
+import com.example.danceit.Model.User;
+import com.example.danceit.Model.Video;
+import com.example.danceit.RecyclerViewComponents.Firebase_RecyclerViewAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class LibrarySearchActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Firebase_RecyclerViewAdapter adapter;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_search);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        CollectionReference reference = database.collection("video_urls_private").
+                document(Objects.requireNonNull(Objects.requireNonNull(mAuth.getCurrentUser()).getEmail())).
+                collection("private_video");
+
+
+        // Getting intent, user search query and all the videos in the user's library from the Main Activity
+        Intent intent = getIntent();
+        String [] searchKeywords = intent.getExtras().getStringArray("Search Keywords");
+        ArrayList<Video> allVideos = intent.getExtras().getParcelableArrayList("Videos");
+        System.out.println("hshjdhksjdjckjskdjkjdkjcksdjck");
+        System.out.println(allVideos.size());
+
+        //Create Search object to get search results and set the adapter
+        Search search = new Search (searchKeywords, allVideos);
+        ArrayList<Video> searchResults = search.searchResults();
+        System.out.println(searchResults.size());
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        Query query = reference.limit(100);
+
+        //Query query = reference.whereArrayContainsAny("tags", searchResults).limit(100);
+
+        FirestoreRecyclerOptions<Video> options = new FirestoreRecyclerOptions.Builder<Video>()
+                .setQuery(query, Video.class)
+                .build();
+        adapter = new Firebase_RecyclerViewAdapter(options, this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getApplicationContext()));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();;
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
