2e0d6c8f892acad9f760f9e3c8f3db61a9103b04
bohlalemotsieloa <bohlalemotsieloa@gmail.com>
2020-10-13 06:25:32 +0200

    Query the database with the search results videoId's.

--- app/src/main/java/com/example/danceit/LibrarySearchActivity.java
+++ app/src/main/java/com/example/danceit/LibrarySearchActivity.java
@@ -7,39 +7,21 @@
  */
 package com.example.danceit;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import com.example.danceit.Model.Search;
-import com.example.danceit.Model.Tag;
-import com.example.danceit.Model.User;
 import com.example.danceit.Model.Video;
 import com.example.danceit.RecyclerViewComponents.Firebase_RecyclerViewAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class LibrarySearchActivity extends AppCompatActivity {
@@ -68,18 +50,14 @@ public class LibrarySearchActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String [] searchKeywords = intent.getExtras().getStringArray("Search Keywords");
         ArrayList<Video> allVideos = intent.getExtras().getParcelableArrayList("Videos");
-        System.out.println("hshjdhksjdjckjskdjkjdkjcksdjck");
-        System.out.println(allVideos.size());
 
         //Create Search object to get search results and set the adapter
         Search search = new Search (searchKeywords, allVideos);
-        ArrayList<Video> searchResults = search.searchResults();
-        System.out.println(searchResults.size());
+        ArrayList<String> searchResults = search.searchResults();
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        Query query = reference.limit(100);
 
-        //Query query = reference.whereArrayContainsAny("tags", searchResults).limit(100);
+        Query query = reference.whereIn("videoId", searchResults);
 
         FirestoreRecyclerOptions<Video> options = new FirestoreRecyclerOptions.Builder<Video>()
                 .setQuery(query, Video.class)
