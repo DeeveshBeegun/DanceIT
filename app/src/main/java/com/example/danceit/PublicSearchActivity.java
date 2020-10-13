86aea9fad669fc088fd041793aba2d1106738f8d
bohlalemotsieloa <bohlalemotsieloa@gmail.com>
2020-10-13 06:28:06 +0200

    Query the database with the search results videoId's for display by adapter.

--- app/src/main/java/com/example/danceit/PublicSearchActivity.java
+++ app/src/main/java/com/example/danceit/PublicSearchActivity.java
@@ -9,17 +9,21 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.danceit.Model.Search;
-import com.example.danceit.Model.Tag;
-import com.example.danceit.Model.User;
+
 import com.example.danceit.Model.Video;
 import com.example.danceit.RecyclerViewComponents.Firebase_RecyclerViewAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class PublicSearchActivity extends AppCompatActivity {
-
-    Firebase_RecyclerViewAdapter mAdapter;
+    RecyclerView recyclerView;
+    Firebase_RecyclerViewAdapter adapter;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +32,47 @@ public class PublicSearchActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // Getting intent, user search query and all the videos in the user's library from the Main Activity
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        CollectionReference reference = database.collection("video_url");
+
+
+        // Getting intent, user search query and all the videos in the public database from the Main Activity
         Intent intent = getIntent();
-        String[] searchKeywords = intent.getExtras().getStringArray("Search Keywords");
+        String [] searchKeywords = intent.getExtras().getStringArray("Search Keywords");
         ArrayList<Video> allVideos = intent.getExtras().getParcelableArrayList("Videos");
 
         //Create Search object to get search results and set the adapter
         Search search = new Search (searchKeywords, allVideos);
-        //System.out.println("Just before searchResults1");
-        ArrayList<Video> searchResults = search.searchResults();
+        ArrayList<String> searchResults = search.searchResults();
 
-        //mAdapter = new RecyclerViewAdapter(searchResults);
-        //RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        //recyclerView.setAdapter(mAdapter);
-        //recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        Query query = reference.whereIn("videoId", searchResults);
+
+        FirestoreRecyclerOptions<Video> options = new FirestoreRecyclerOptions.Builder<Video>()
+                .setQuery(query, Video.class)
+                .build();
+
+        adapter = new Firebase_RecyclerViewAdapter(options, this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getApplicationContext()));
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
 }
